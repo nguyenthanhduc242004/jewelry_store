4f285e2df0ad53c07ccc69b7b400444a968ed33e
Duc <nguyenthanhduc242004@gmail.com>
2025-11-26 10:08:14 +0700

    added nav

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -17,6 +17,8 @@ declare module 'vue' {
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
     HeartIcon: typeof import('./src/components/icon/HeartIcon.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,11 +2,19 @@
 import { Goods, Search, User } from "@element-plus/icons-vue";
 import { ref } from "vue";
 import HeartIcon from "@/components/icon/HeartIcon.vue";
+import type { TabsPaneContext } from "element-plus";
+import { RouterLink } from "vue-router";
+
 const input = ref();
+const activeTab = ref();
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event);
+};
 </script>
 
 <template>
-  <header style="display: flex; align-items: center; padding: 0 20px">
+  <header style="display: flex; align-items: center">
     <el-input
       v-model="input"
       placeholder="What are you looking for?"
@@ -30,5 +38,57 @@ const input = ref();
       </el-badge>
     </div>
   </header>
-  <el-divider style="margin: 0" />
+  <el-divider style="margin: 4px 0" />
+  <nav>
+    <ul style="list-style: none; display: flex; justify-content: center">
+      <li>
+        <RouterLink class="nav-item" to="/brands">Brands</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/earrings">Earrings</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/necklaces">Necklaces</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/rings">Rings</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/bracelets">Bracelets</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/gifts">Gifts</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-item" to="/personalized">
+          Personalized
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style scoped>
+.nav-item {
+  display: block;
+  height: 40px;
+  line-height: 40px;
+  text-decoration: none;
+  color: #555;
+  padding: 0 20px;
+  font-size: 18px;
+
+  &:hover {
+    animation: navItemColorChange 0.2s forwards;
+  }
+}
+
+@keyframes navItemColorChange {
+  from {
+    color: #555;
+  }
+  to {
+    color: #ffc108;
+  }
+}
+</style>
