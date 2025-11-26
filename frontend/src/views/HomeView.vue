a39b0305bcbcc89cf8dd796e198c72df9006496e
Duc <nguyenthanhduc242004@gmail.com>
2025-11-26 08:58:11 +0700

    project init

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+import { ref } from "vue";
+
+const input = ref();
+</script>
+
+<template>
+  <header>
+    <el-input v-model="input" placeholder="What are you looking for?">
+      <template #prefix>
+        <el-icon class="el-input__icon"><Search /></el-icon>
+      </template>
+    </el-input>
+  </header>
+</template>
